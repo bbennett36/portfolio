c60e91ad64feec2f6f24f5920ab5bd112a496856
Brennan <bennettglob@gmail.com>
2016-07-30 00:51:24 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to my Portfilio!</h2>
+        <navbarInstance />
+
+        </div>
+        <p className="App-intro">
+        My Name is Brennan Bennett
+        </p>
+      </div>
+    );
+  }
+}
+
+
+const navbarInstance =  React.createClass({
+  render: function() {
+   return (
+  <Navbar inverse>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">Link</NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+}
+});
+
+export default App;
